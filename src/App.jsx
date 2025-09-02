2908303bb64569e2723babf1edc7ecd1ccfd3028
neelanjan14 <64602911+neelanjan14@users.noreply.github.com>
2025-09-03 00:04:16 +0530

    Add main application component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThreeDContainer from './components/ThreeDContainer';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Interactive Portfolio</h1>
+      <ThreeDContainer />
+    </div>
+  );
+};
+
+export default App;
